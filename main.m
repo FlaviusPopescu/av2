b7e8d58e62172a7eafe30e3b0df05c31d33eeeae
Flavius Popescu <flavius.popescu@gmail.com>
2013-02-28 16:18:08 +0000

    fixed euclidean distance

--- main.m
+++ main.m
@@ -77,14 +77,14 @@ function main(skip_detection)
             cr = biggest_center(R);
             tracks(ii, :) = [cy, cb, cr];
             
-            set(fg, 'name', files(ii).name);
-            imshow(R + Y + B);
-            hold on
-            plot(cy(1), cy(2), 'y*', cb(1), cb(2), 'b*', cr(1), cr(2), 'r*');
-            drawnow;
+            % set(fg, 'name', files(ii).name);
+            % imshow(R + Y + B);
+            % hold on
+            % plot(cy(1), cy(2), 'y*', cb(1), cb(2), 'b*', cr(1), cr(2), 'r*');
+            % drawnow;
 						
 						
-            pause(1 - toc)
+            % pause(1 - toc)
             perc = ii/(size(files,1) * 2 + 4);
             waitbar(perc,wb,sprintf('%d%% completed...',round(perc * 100)));
         end 
@@ -96,30 +96,30 @@ function main(skip_detection)
     % Tracking is largely done in the previous step, here we visualise the
     % individual required images as well as an overall image of the
     % juggling.
-    figure(1);
-    imshow(imread('background.jpg'))
-    hold on
-    plot(tracks(:, 1), tracks(:, 2), 'y', tracks(:, 3), ...
-        tracks(:, 4), 'b', tracks(:, 5), tracks(:, 6), 'r')
-    print('-dpng', 'report/tracking')
-    
-    figure(1);
-    imshow(imread('background.jpg'))
-    hold on
-    plot(tracks(:, 1), tracks(:, 2), 'y')
-    print('-dpng', 'report/tracking-yellow')
-    
-    figure(1);
-    imshow(imread('background.jpg'))
-    hold on
-    plot(tracks(:, 3), tracks(:, 4), 'b')
-    print('-dpng', 'report/tracking-blue')
-    
-    figure(1);
-    imshow(imread('background.jpg'))
-    hold on
-    plot(tracks(:, 5), tracks(:, 6), 'r')
-    print('-dpng', 'report/tracking-red')
+    % figure(1);
+    % imshow(imread('background.jpg'))
+    % hold on
+    % plot(tracks(:, 1), tracks(:, 2), 'y', tracks(:, 3), ...
+    %     tracks(:, 4), 'b', tracks(:, 5), tracks(:, 6), 'r')
+    % print('-dpng', 'report/tracking')
+    % 
+    % figure(1);
+    % imshow(imread('background.jpg'))
+    % hold on
+    % plot(tracks(:, 1), tracks(:, 2), 'y')
+    % print('-dpng', 'report/tracking-yellow')
+    % 
+    % figure(1);
+    % imshow(imread('background.jpg'))
+    % hold on
+    % plot(tracks(:, 3), tracks(:, 4), 'b')
+    % print('-dpng', 'report/tracking-blue')
+    % 
+    % figure(1);
+    % imshow(imread('background.jpg'))
+    % hold on
+    % plot(tracks(:, 5), tracks(:, 6), 'r')
+    % print('-dpng', 'report/tracking-red')
        
     
     % Task 3: Evaluation
@@ -131,9 +131,16 @@ function main(skip_detection)
     ev = load('gt1.mat');
     gt = ev.gt1';
     
-    yellow_d = sqrt(sum(gt(:,[7,6]) - tracks(:,[1,2]), 2) .^ 2);
-    blue_d   = sqrt(sum(gt(:,[5,4]) - tracks(:,[3,4]), 2) .^ 2);
-    red_d    = sqrt(sum(gt(:,[3,2]) - tracks(:,[5,6]), 2) .^ 2);
+		% gt is   3, 2 red    4, 5   blue    6 7 yellow
+		% tracks is  1 2 yellow 3,4 blue     5,6 red
+
+		yellow_d = sqrt( (gt(:,7) - tracks(:,1) ).^2 + (gt(:,6) - tracks(:,2)).^2    );
+		blue_d = sqrt(  (gt(:,5) - tracks(:,3) ).^2 + (gt(:,4) - tracks(:,4)).^2     );
+		red_d = sqrt(  (gt(:,3) - tracks(:,5) ).^2 + (gt(:,2) - tracks(:,6) ) .^2) ;
+		
+    % yellow_d = sqrt(sum(  (gt(:,[7,6]) - tracks(:,[1,2]) ) .^ 2 , 2)  );
+    % blue_d   = sqrt(sum(  (gt(:,[5,4]) - tracks(:,[3,4]) ) .^ 2 , 2)  );
+    % red_d    = sqrt(sum(  (gt(:,[3,2]) - tracks(:,[5,6]) ) .^ 2 , 2)  );
     
     yellow_correct = sum(yellow_d < 10);
     blue_correct   = sum(blue_d < 10);
@@ -166,7 +173,7 @@ function main(skip_detection)
             gt(ii,5), gt(ii, 4), 'b*', ...
             gt(ii,3), gt(ii,2), 'r*');
         drawnow;
-        pause(1 - toc)
+        % pause(1 - toc)
         perc = (ii + size(files,1))/(size(files,1) * 2 + 4);
         
         [pathstr, name, ext] = fileparts(files(ii).name);
